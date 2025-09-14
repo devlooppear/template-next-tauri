f44742c3195fb2fc73faf1c09be936efad649b2a
devlooppear <iago.profissional.developer@gmail.com>
2025-09-14 18:35:47 -0300

    optimize

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,26 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public/build-pwa",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   typescript: {
     ignoreBuildErrors: true,
   },
-
   staticPageGenerationTimeout: 960,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
